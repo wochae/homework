4d8360156a6fb9a603faa7a56118190c0b2a5957
wochae <101040879+wochae@users.noreply.github.com>
2022-03-20 16:45:28 +0900

    Create ft_lstclear.c

---
+++ libft/ft_lstclear.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wochae <wochae@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/20 16:44:35 by wochae            #+#    #+#             */
+/*   Updated: 2022/03/20 16:44:35 by wochae           ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list *to_del;
+
+	if (!lst || !del)
+		return;
+	while ((*lst))
+	{
+		to_del = *lst;
+		*lst = (*lst)->next;
+		ft_lstdelone(to_del, del);
+	}
+}
