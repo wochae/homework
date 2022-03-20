822b7ba802c6779a883aebb4150eda7ebb0b63b7
wochae <101040879+wochae@users.noreply.github.com>
2022-03-20 20:38:29 +0900

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: wochae <wochae@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 18:23:19 by wochae            #+#    #+#             */
-/*   Updated: 2022/03/19 18:29:38 by wochae           ###   ########.fr       */
+/*   Updated: 2022/03/20 19:26:27 by wochae           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,28 +20,28 @@ typedef struct s_list
 {
 	void			*content;
 	struct s_list	*next;
-}	t_list;
+}					t_list;
 
-// int		ft_atoi(const char *str);
+int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
-// void	*ft_calloc(size_t cnt, size_t n);
+void	*ft_calloc(size_t cnt, size_t n);
 // char	*ft_itoa(int n);
-// void	ft_lstclear(t_list **lst, void (*del)(void *));
-// void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 // void	ft_lstiter(t_list *lst, void (*f)(void *));
-// t_list	*ft_lstnew(void *content);
-// void	ft_lstadd_back(t_list **lst, t_list *new);
-// void	ft_lstadd_front(t_list **lst, t_list *new);
-// t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+t_list	*ft_lstnew(void *content);
+t_list	*ft_lstlast(t_list *lst);
 // t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void*));
-// int		ft_lstsize(t_list *lst);
-// void	*ft_memchr(const void *s, int c, size_t n);
-// int		ft_memcmp(const void *s1, const void *s2, size_t n);
+int		ft_lstsize(t_list *lst);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memset(void *s, int c, size_t n);
