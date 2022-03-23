b8e8d10a093b90fad29e2a41e28c14d51e222c09
wochae <101040879+wochae@users.noreply.github.com>
2022-03-23 21:16:28 +0900

    before I turn in git push my repo

--- libft/ft_lstiter.c
+++ libft/ft_lstiter.c
@@ -6,19 +6,13 @@
 /*   By: wochae <wochae@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/20 19:46:45 by wochae            #+#    #+#             */
-/*   Updated: 2022/03/23 19:47:32 by wochae           ###   ########.fr       */
+/*   Updated: 2022/03/20 20:14:56 by wochae           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void	(*f)(void *))
+void	ft_lsttier(t_list *lst, void	(*f)(void *))
 {
-	if (!lst || !f)
-		return ;
-	while (lst)
-	{
-		f(lst->content);
-		lst = lst->next;
-	}
+
 }
