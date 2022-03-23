0b6556f4a4a034a9425fb4f57d763e55db427365
wochae <101040879+wochae@users.noreply.github.com>
2022-03-23 17:00:30 +0900

    testing to check diff from changing another file.

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -5,16 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: wochae <wochae@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/16 21:58:54 by wochae            #+#    #+#             */
-/*   Updated: 2022/03/19 18:27:17 by wochae           ###   ########.fr       */
+/*   Created: 2022/03/20 17:30:25 by wochae            #+#    #+#             */
+/*   Updated: 2022/03/23 15:01:15 by wochae           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bzero(void	*s, size_t	n)
+void ft_bzero(void *s, size_t n)
 {
-	size_t	i;
+	size_t i;
 
 	i = 0;
 	while (i < n)
