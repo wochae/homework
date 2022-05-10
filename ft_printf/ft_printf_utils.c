3a160625c4f4de4460f1fb8f5a4483c044d48294
wochae <101040879+wochae@users.noreply.github.com>
2022-05-10 14:56:51 +0900

    Add files via upload

---
+++ ft_printf/ft_printf_utils.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wochae <wochae@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/10 13:40:45 by wochae            #+#    #+#             */
+/*   Updated: 2022/05/01 01:20:12 by wochae           ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int putarg_cnt(va_list *ap, char type)
+{
+	int cnt;
+
+	cnt = 0;
+	if (type == '%')
+	{
+		cnt = ft_putchr_cnt('%');
+	}
+	else if (type == 'c')
+	{
+		cnt = ft_putchr_cnt(va_arg(*ap, int));
+	}
+	else if (type == 's')
+	{
+		cnt = ft_putstr_cnt(va_arg(*ap, char *));
+	}
+	else if (type == 'p')
+	{
+		cnt = ft_putstr_cnt("0x") + putnbr_type((long)va_arg(*ap, void *), type);
+	}
+	else
+	{
+		cnt = putnbr_type(va_arg(*ap, int), type);
+	}
+	return (cnt);
+}
+
+int putnbr_type(long nbr, char type)
+{
+	t_notation notation;
+	int cnt;
+
+	cnt = 0;
+	init_struct(&notation, type);
+
+	if (ft_strchr(SIGNED, type) && nbr < 0)
+	{
+		cnt = cnt + ft_putchr_cnt('-');
+		nbr = -nbr;
+	}
+
+	if (ft_strchr(INT, type))
+	{
+		putnbr_int(nbr, notation, &cnt);
+	}
+	else if (ft_strchr(LONG, type))
+	{
+		putnbr_long(nbr, notation, &cnt);
+	}
+	return (cnt);
+}
+
+void init_struct(t_notation *notation, char type)
+{
+	notation->base = 10;
+	notation->captial = 0;
+	if (ft_strchr(HEXA, type))
+	{
+		notation->base = 16;
+	}
+	if (ft_strchr(CAPITAL, type))
+	{
+		notation->capital = 16;
+	}
+}
+
+void putnbr_int(unsigned int nbr, t_notation notation, int *cnt)
+{
+	if (nbr < notation.base)
+	{
+		*cnt = *cnt + ft_putchr_cnt(DIGITS[nbr + notation.capital])
+	}
+	else
+	{
+		putnbr_int(nbr / notation.base, notation, cnt);
+		putnbr_int(nbr % notation.base, notation, cnt);
+	}
+}
+
+void putnbr_long(unsigned long nbr, t_notation notation, int *cnt)
+{
+	if (nbr < notation.base)
+	{
+		*cnt = *cnt + ft_putchr_cnt(DIGITS[nbr + notation.capital])
+	}
+	else
+	{
+		putnbr_long(nbr / notation.base, notation, cnt);
+		putnbr_long(nbr % notation.base, notation, cnt);
+	}
+}
